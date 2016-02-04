0546f177e19063b1eee42fbc49c8ae2dea3e2703
Michael Jackson <mike.jackson@bluequartz.net>
2016-02-04 13:11:06 -0500

    Updates to CMake for latest repo reorg. DevHelper should now be able to generate a compilable plugin for SIMPLView/DREAM3D

--- Source/Applications/DevHelper/PMFileGenerator.cpp
+++ Source/Applications/DevHelper/PMFileGenerator.cpp
@@ -333,16 +333,8 @@ QString PMFileGenerator::createReplacementString(FileType type, QSet<QString> na
       {
         name.replace("@PluginName@", pluginName);
       }
-
-      rsOut << "AddSIMPLViewUnitTest(TESTNAME " << name << "Test\n";
-      rsOut << "                   SOURCES ${${PLUGIN_NAME}Test_SOURCE_DIR}/" + name + "Test.cpp\n";
-      rsOut << "                   FOLDER \"${PLUGIN_NAME}Plugin/Test\"\n";
-      rsOut << "                   LINK_LIBRARIES ${${PLUGIN_NAME}_Link_Libs})\n";
-
-      if (++iter != names.end())
-      {
-        replaceStr.append("\n");
-      }
+      rsOut << "  " << name << "\n";
+      ++iter;
     }
   }
   else if (type == TESTFILELOCATIONS)
