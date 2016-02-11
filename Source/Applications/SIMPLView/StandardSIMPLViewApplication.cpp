1bceb5b4ee88716fbd803d42d589477fddd74ed2
Joey Kleingers <joey.kleingers@bluequartz.net>
2016-02-11 10:51:27 -0500

    Fixing bug where the toolbox does not always remember its visibility state between runs of the DREAM3D application on Windows.

--- Source/Applications/SIMPLView/StandardSIMPLViewApplication.cpp
+++ Source/Applications/SIMPLView/StandardSIMPLViewApplication.cpp
@@ -61,7 +61,10 @@ StandardSIMPLViewApplication::StandardSIMPLViewApplication(int& argc, char** arg
 // -----------------------------------------------------------------------------
 StandardSIMPLViewApplication::~StandardSIMPLViewApplication()
 {
-
+#if defined(Q_OS_WIN)
+  SIMPLViewToolbox* toolbox = SIMPLViewToolbox::Instance();
+  toolbox->writeSettings();
+#endif
 }
 
 // -----------------------------------------------------------------------------
