7ac7b7ad94bc8eb3c021820eac730ed79a1e5c7a
Michael Jackson <mike.jackson@bluequartz.net>
2018-01-15 15:51:16 -0500

    Updating Filter templates for DevHelper to latest APIs

--- Source/DevHelper/PMFileGenerator.cpp
+++ Source/DevHelper/PMFileGenerator.cpp
@@ -41,8 +41,9 @@
 #include <QtCore/QDir>
 #include <QtCore/QFile>
 #include <QtCore/QFileInfo>
-#include <QtCore/QTextStream>
 #include <QtCore/QFileInfo>
+#include <QtCore/QTextStream>
+#include <QtCore/QUuid>
 
 #include "SVWidgetsLib/QtSupport/QtSApplicationFileInfo.h"
 
@@ -226,6 +227,23 @@ QString PMFileGenerator::getFileContents(QString replaceStr)
     text.replace("@ClassNameLowerCase@", className.toLower());
     text.replace("@FilterGroup@", pluginName);
     text.replace("@FilterSubgroup@", pluginName);
+
+    uint l = 100;
+    ushort w1 = 200;
+    ushort w2 = 300;
+
+    QString libName = pluginName;
+    uchar b[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    int32_t i = 0;
+    while(i < 8 && i < libName.size())
+    {
+      b[i] = static_cast<uint8_t>(libName.at(i).toLatin1());
+      i++;
+    }
+    QUuid uuid = QUuid(l, w1, w2, b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]);
+    QString nameSpace = QString("%1 %2").arg(className).arg(pluginName);
+    QUuid p1 = QUuid::createUuidV5(uuid, nameSpace);
+    text.replace("@Uuid@", p1.toString());
 
     if (replaceStr.isEmpty() == false)
     {
