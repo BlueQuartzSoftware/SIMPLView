cdb47a4bbe02d830e5fadbd0325eff93b277abc3
Michael Jackson <mike.jackson@bluequartz.net>
2018-12-20 16:37:00 -0500

    StyleSheetEditor style and code cleanup
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Source/SIMPLView/StyleSheetEditor.cpp
+++ Source/SIMPLView/StyleSheetEditor.cpp
@@ -69,10 +69,11 @@ StyleSheetEditor::StyleSheetEditor(QWidget* parent)
 
   connect(&m_FileWatcher, SIGNAL(fileChanged(const QString&)), this, SLOT(qssFileChanged(const QString&)));
 
-  QString styleSheetPath = QString("%1/%2").arg(QDir::homePath(), 1).arg("DREAM3D-Dev/DREAM3D/ExternalProjects/BrandedDREAM3D/DREAM3D/StyleSheets/Dark.json");
+  QString styleSheetPath = QString("%1/%2").arg(QDir::homePath(), 1).arg("DREAM3D-Dev/DREAM3D/ExternalProjects/BrandedDREAM3D/DREAM3D/StyleSheets/Midnight.json");
   m_Ui->jsonFilePath->setText(styleSheetPath);
-  styleSheetPath = QString("%1/%2").arg(QDir::homePath(), 1).arg("DREAM3D-Dev/DREAM3D/ExternalProjects/BrandedDREAM3D/DREAM3D/StyleSheets/dark.css");
+  styleSheetPath = QString("%1/%2").arg(QDir::homePath(), 1).arg("DREAM3D-Dev/DREAM3D/ExternalProjects/BrandedDREAM3D/DREAM3D/StyleSheets/Midnight.css");
   m_Ui->cssFilePath->setText(styleSheetPath);
+  on_reloadButton_stateChanged(Qt::Checked);
 }
 
 // -----------------------------------------------------------------------------
