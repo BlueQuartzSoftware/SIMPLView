60c3d98f1f97f92112541483aeea7d7e4a536033
Joey Kleingers <joey.kleingers@bluequartz.net>
2019-04-05 12:59:32 -0400

    Clang format SIMPLViewUIMessageHandler.h

--- Source/SIMPLView/SIMPLViewUIMessageHandler.h
+++ Source/SIMPLView/SIMPLViewUIMessageHandler.h
@@ -1,42 +1,42 @@
 /* ============================================================================
-* Copyright (c) 2009-2016 BlueQuartz Software, LLC
-*
-* Redistribution and use in source and binary forms, with or without modification,
-* are permitted provided that the following conditions are met:
-*
-* Redistributions of source code must retain the above copyright notice, this
-* list of conditions and the following disclaimer.
-*
-* Redistributions in binary form must reproduce the above copyright notice, this
-* list of conditions and the following disclaimer in the documentation and/or
-* other materials provided with the distribution.
-*
-* Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
-* contributors may be used to endorse or promote products derived from this software
-* without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-* The code contained herein was partially funded by the followig contracts:
-*    United States Air Force Prime Contract FA8650-07-D-5800
-*    United States Air Force Prime Contract FA8650-10-D-5210
-*    United States Prime Contract Navy N00173-07-C-2068
-*
-* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+ * Copyright (c) 2009-2016 BlueQuartz Software, LLC
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
+ * contributors may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The code contained herein was partially funded by the followig contracts:
+ *    United States Air Force Prime Contract FA8650-07-D-5800
+ *    United States Air Force Prime Contract FA8650-10-D-5210
+ *    United States Prime Contract Navy N00173-07-C-2068
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 #pragma once
 
-#include "SIMPLib/SIMPLib.h"
 #include "SIMPLib/Messages/AbstractMessageHandler.h"
+#include "SIMPLib/SIMPLib.h"
 
 class SIMPLView_UI;
 class AbstractStatusMessage;
@@ -47,38 +47,36 @@ class AbstractStatusMessage;
  */
 class SIMPLib_EXPORT SIMPLViewUIMessageHandler : public AbstractMessageHandler
 {
-  public:
-    explicit SIMPLViewUIMessageHandler(SIMPLView_UI* uiWidget);
+public:
+  explicit SIMPLViewUIMessageHandler(SIMPLView_UI* uiWidget);
 
-    /**
-     * @brief Sets the SIMPLView_UI status bar and appends the standard output widget with
-     * incoming FilterStatusMessage's status message.
-     * @param msg
-     */
-    void processMessage(const FilterStatusMessage* msg) const override;
+  /**
+   * @brief Sets the SIMPLView_UI status bar and appends the standard output widget with
+   * incoming FilterStatusMessage's status message.
+   * @param msg
+   */
+  void processMessage(const FilterStatusMessage* msg) const override;
 
-    /**
-     * @brief Sets the SIMPLView_UI progress bar with the incoming PipelineProgressMessage's
-     * progress value.
-     * @param msg
-     */
-    void processMessage(const PipelineProgressMessage* msg) const override;
+  /**
+   * @brief Sets the SIMPLView_UI progress bar with the incoming PipelineProgressMessage's
+   * progress value.
+   * @param msg
+   */
+  void processMessage(const PipelineProgressMessage* msg) const override;
 
-    /**
-     * @brief Sets the SIMPLView_UI status bar and appends the standard output widget with
-     * incoming PipelineStatusMessage's status message.
-     * @param msg
-     */
-    void processMessage(const PipelineStatusMessage* msg) const override;
+  /**
+   * @brief Sets the SIMPLView_UI status bar and appends the standard output widget with
+   * incoming PipelineStatusMessage's status message.
+   * @param msg
+   */
+  void processMessage(const PipelineStatusMessage* msg) const override;
 
-  private:
-    SIMPLView_UI* m_UIWidget = nullptr;
+private:
+  SIMPLView_UI* m_UIWidget = nullptr;
 
-    /**
-     * @brief processStatusMessage
-     * @param msg
-     */
-    void appendStatusMessageToPipelineOutput(const QString &statusMessage) const;
+  /**
+   * @brief processStatusMessage
+   * @param msg
+   */
+  void appendStatusMessageToPipelineOutput(const QString& statusMessage) const;
 };
-
-
